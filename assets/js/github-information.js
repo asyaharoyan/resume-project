bfe2d4b7a3fcbbc13c58e798836f9fcb4f46bfcf
Asya Haroyan <alharoyan@gmail.com>
2024-01-31 11:42:04 +0100

    fixing bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -48,7 +48,7 @@ function fetchGitHubInformation(event) {
     }
     $("#gh-user-data").html(`
     <div id="loader">
-        <img src="assets/css/loader.gif" alt="loading..."/>
+    <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
@@ -60,12 +60,16 @@ function fetchGitHubInformation(event) {
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-        }, function(errorResponce) {
-            if (errorResponce.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found user ${username}</h2>`);
+        }, 
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
-                console.log(errorResponce);
-                $("#gh-user-data").html(`<h2>Error: ${errorResponce.responseJSON.message}</h2>`);
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         }
     );
